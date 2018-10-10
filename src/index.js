99419c8ec33e89331eddaabf309a52806988507a
dominickenorwood <dominickenorwood@gamil.com>
2018-10-10 14:52:33 -0400

    Added Clarafai JS client and basic api call and response

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+// https://clarifai.com/developer/guide/predict#images
+import Clarifai from 'clarifai';
 import './index.css';
 
-console.log('AI App Activate')
+const app = new Clarifai.App({
+    apiKey: '0675b1655d95447b95caaf6ac602428c'
+});
+
+app.models.predict(Clarifai.FACE_DETECT_MODEL, 'https://face-negotiationtheory.weebly.com/uploads/4/2/1/6/4216257/1771161.jpg')
+    .then(
+        response => {
+            console.log('Response', response)
+        },
+        error => {
+            console.log('error', error)
+        }
+    )
